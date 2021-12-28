76bc8b0074fe8c16f8e2cb9bbf97c5ab4eb696fa
DraimCiDo <danilaorlov4@gmail.com>
2021-12-28 23:55:08 +0200

    [1.0.10] - Добавление подхвата команд /draimdonate - Добавление считавания параметров из config.yml - Изменение функций getInstance и getQiwi - Смещение открытие мейн-класса. - Добавление новых импортов и изменение старых.

--- src/main/java/me/draimgoose/DraimDonate.java
+++ src/main/java/me/draimgoose/DraimDonate.java
@@ -1,22 +1,51 @@
 package me.draimgoose;
 
+import me.draimgoose.Commands.AdminGUICommand;
+import me.draimgoose.Commands.TabComplete;
 import me.draimgoose.Config.DBCore;
 import me.draimgoose.Config.MainConfig;
 import me.draimgoose.Config.MessageConfig;
 import me.draimgoose.Utils.ColorUtils;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DraimDonate extends JavaPlugin {
+import java.util.List;
+import java.util.Set;
 
+public final class DraimDonate extends JavaPlugin
+{
+    public static FileConfiguration config;
+    public static NamespacedKey buttonKey;
+    public static NamespacedKey AdminGUI;
     private static DraimDonate instance;
     private static String QiWiT;
 
     public static DraimDonate getInstance() {
-        return DraimDonate.instance;
+        return instance;
+    }
+    public static String getQiwi() {
+        return QiWiT;
     }
 
-    public static String getQiwi() {
-        return DraimDonate.QiWiT;
+    // Подхват всех настроек из config.yml
+    public static String getConfigString(String path) {
+        return config.getString(path);
+    }
+    public static int getConfigInt(String path) {
+        return config.getInt(path);
+    }
+    public static Set<String> getConfigKeys(String path, boolean deep) {
+        return config.getConfigurationSection(path).getKeys(deep);
+    }
+    public static List<Integer> getConfigIntList(String path) {
+        return config.getIntegerList(path);
+    }
+    public static Boolean getConfigBoolean(String path) {
+        return config.getBoolean(path);
+    }
+    public static List<String> getConfigStringList(String path) {
+        return config.getStringList(path);
     }
 
     public void onEnable() {
@@ -41,6 +70,14 @@ public final class DraimDonate extends JavaPlugin {
         DraimDonate.QiWiT = MainConfig.getMain().getCFG().getString("DraimDonate.Token");
     }
 
+    // Команда "/draimdonate" выводит GUI-панель которая показывает админ функционал плагина
+    // Такой как, доната, даты пополнения и т.д.
+    public void initCMDs() {
+        getCommand("draimdonate").setExecutor(new AdminGUICommand(this));
+        getCommand("draimdonate").setTabCompleter(new TabComplete());
+    }
+
+    // Принт в Консоль, просто для красоты
     public void printASCII() {
         getLogger().info(ColorUtils.colorMessage(" &6 /$$$$$$$                     /$$               /$$$$$$$                                  /$$              "));
         getLogger().info(ColorUtils.colorMessage(" &6| $$__  $$                   |__/              | $$__  $$                                | $$              "));
