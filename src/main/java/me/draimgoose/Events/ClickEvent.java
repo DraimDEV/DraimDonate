7d400a16be96f0b3e3b3c81f3be2a388bea97691
DraimCiDo <danilaorlov4@gmail.com>
2022-01-16 21:04:29 +0300

    [1.0.53] Создание ивента на клик

---
+++ src/main/java/me/draimgoose/Events/ClickEvent.java
@@ -0,0 +1,43 @@
+package me.draimgoose.Events;
+
+import me.draimgoose.DraimDonate;
+import me.draimgoose.GUIs.AdminGUI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.persistence.PersistentDataType;
+
+public class ClickEvent implements Listener {
+
+    @EventHandler(ignoreCancelled = true)
+    public void onClick(InventoryClickEvent e) {
+
+        if (e.getCurrentItem() == null) {
+            return;
+        }
+
+        Player p = (Player) e.getWhoClicked();
+
+        if (!p.getPersistentDataContainer().has(DraimDonate.AdminGUI, PersistentDataType.STRING)) {
+            return;
+        }
+        else if (!p.getPersistentDataContainer().get(DraimDonate.AdminGUI, PersistentDataType.STRING).equalsIgnoreCase(AdminGUI.name)) {
+            return;
+        }
+
+        e.setCancelled(true);
+
+        if (e.getClickedInventory().getHolder() != null) {
+            if (e.getClickedInventory().getHolder().equals(e.getWhoClicked())) return;
+        }
+
+        switch (e.getCurrentItem().getItemMeta().getPersistentDataContainer().get(DraimDonate.buttonKey, PersistentDataType.STRING)) {
+            case ("EXIT"):
+                p.closeInventory();
+                break;
+            default:
+                break;
+        }
+    }
+}
